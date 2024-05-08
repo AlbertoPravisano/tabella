d1114f62ecd78aba66c57793a12f5628eba2a758
Alberto Pravisano <alberto.pravisano@beantech.it>
2024-05-08 15:55:12 +0200

    modifiche richieste

--- src/App.js
+++ src/App.js
@@ -5,35 +5,48 @@ import {
   PlusOutlined,
 } from "@ant-design/icons";
 import { Button } from "antd";
-import { Container, Divider, Button as UiButton } from "semantic-ui-react";
+import {
+  Container,
+  Divider,
+  Header,
+  Icon,
+  Button as UiButton,
+  Form,
+  Input,
+} from "semantic-ui-react";
 import TabellaOrdini from "./TabellaOrdini";
 
-const generateNewRow = (key = 1, color = "") => ({
+const addEmptyOrder = (key = 1, color = "") => ({
   key,
-  name: "",
-  email: "",
+  product: "",
   color,
   code: "",
   quantity: 1,
   note: "",
 });
 
+const INITIAL_STATE = {
+  name: "",
+  emails: [""],
+  orders: [addEmptyOrder()],
+};
+
 const App = () => {
-  const [dataSource, setDataSource] = useState([generateNewRow()]);
+  const [dataSource, setDataSource] = useState(INITIAL_STATE);
   const [isPreventivo, setIsPreventivo] = useState(false);
 
   const onExport = () => {
-    const values = dataSource.map(
-      (obj) =>
-        [obj.name, `"${obj.color}"`, obj.code, obj.quantity, obj.note].join(
-          ";"
-        ) +
-        (isPreventivo ? ";SI" : ";NO") +
-        "\n"
-    );
+    const order =
+      `${dataSource.name};` +
+      dataSource.orders.map(
+        (obj) =>
+          [obj.product, obj.color, obj.code, obj.quantity, obj.note].join(";") +
+          (isPreventivo ? ";SI" : ";NO") +
+          "\n"
+      );
     const encodedUri = encodeURI(
-      "data:text/csv;charset=utf-8,Cliente;Colore;Codice_articolo;Quantita;Note;Preventivo\n" +
-        values
+      "data:text/csv;charset=utf-8,Cliente;Prodotto;Colore;Codice_articolo;Quantita;Note;Preventivo\n" +
+        order
     );
 
     console.log(encodedUri);
@@ -42,21 +55,34 @@ const App = () => {
 
   const onSendMail = () => console.log("Invia via email");
 
+  const addNewEmail = () =>
+    setDataSource({ ...dataSource, emails: [...dataSource.emails, ""] });
+
   const onAddRow = () => {
-    setDataSource([
+    const orders = dataSource.orders;
+    setDataSource({
       ...dataSource,
-      generateNewRow(dataSource.length + 1, dataSource[0].color),
-    ]);
+      orders: [...orders, addEmptyOrder(orders.length + 1, orders[0].color)],
+    });
   };
 
   const onRemoveRow = (index) => {
-    setDataSource(dataSource.filter((_, indexDS) => indexDS !== index));
+    setDataSource({
+      ...dataSource,
+      orders: dataSource.orders.filter((_, indexDS) => indexDS !== index),
+    });
   };
 
-  const onChangeValueRow = (index, key, value) => {
-    const newData = [...dataSource];
-    newData[index][key] = value;
-    setDataSource(newData);
+  const onChangeEmail = (index, email) => {
+    const emails = dataSource.emails;
+    emails[index] = email;
+    setDataSource({ ...dataSource, emails });
+  };
+
+  const onChangeValueOrder = (index, key, value) => {
+    const orders = dataSource.orders;
+    orders[index][key] = value;
+    setDataSource({ ...dataSource, orders });
   };
 
   return (
@@ -74,10 +100,62 @@ const App = () => {
           Preventivo
         </UiButton>
       </UiButton.Group>
+
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="user" />
+          Cliente
+        </Header>
+      </Divider>
+
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Field>
+            <label>Nome</label>
+            <Input
+              value={dataSource.name}
+              placeholder="Mario Rossi"
+              onChange={(e) =>
+                setDataSource({ ...dataSource, name: e.target.value })
+              }
+            />
+          </Form.Field>
+          {dataSource.emails.map((email, index) => (
+            <Form.Field key={index}>
+              <label>Email {index + 1}</label>
+              <Input
+                value={email}
+                placeholder="mariorossi@example.com"
+                onChange={(e) => onChangeEmail(index, e.target.value)}
+              />
+            </Form.Field>
+          ))}
+        </Form.Group>
+      </Form>
+      <Button
+        icon={<MailOutlined />}
+        shape="round"
+        style={{ marginTop: "2em" }}
+        disabled={dataSource.emails.length > 2}
+        data-tooltip={
+          dataSource.emails.length > 2 ? "Limite email raggiunto" : undefined
+        }
+        onClick={addNewEmail}
+      >
+        Aggiungi email
+      </Button>
+
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="shopping cart" />
+          Ordine
+        </Header>
+      </Divider>
+
       <TabellaOrdini
-        dataSource={dataSource}
+        ordini={dataSource.orders}
         onRemoveRow={onRemoveRow}
-        onChangeValueRow={onChangeValueRow}
+        onChangeValueRow={onChangeValueOrder}
       />
 
       <Button
@@ -86,7 +164,7 @@ const App = () => {
         style={{ marginTop: "2em" }}
         onClick={onAddRow}
       >
-        Aggiungi riga
+        Aggiungi articolo
       </Button>
       <Button
         type="primary"
--- src/TabellaOrdini.js
+++ src/TabellaOrdini.js
@@ -2,15 +2,14 @@ import { MinusOutlined } from "@ant-design/icons";
 import { Button, Input, InputNumber } from "antd";
 import { Table } from "semantic-ui-react";
 
-const TabellaOrdini = ({ dataSource, onRemoveRow, onChangeValueRow }) => {
+const TabellaOrdini = ({ ordini, onRemoveRow, onChangeValueRow }) => {
   return (
     <Table celled unstackable>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell>Cliente</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
+          <Table.HeaderCell>Articolo</Table.HeaderCell>
           <Table.HeaderCell>Colore</Table.HeaderCell>
-          <Table.HeaderCell>Codice art.</Table.HeaderCell>
+          <Table.HeaderCell>Codice</Table.HeaderCell>
           <Table.HeaderCell>Quantità</Table.HeaderCell>
           <Table.HeaderCell>Note</Table.HeaderCell>
           <Table.HeaderCell></Table.HeaderCell>
@@ -18,11 +17,11 @@ const TabellaOrdini = ({ dataSource, onRemoveRow, onChangeValueRow }) => {
       </Table.Header>
 
       <Table.Body>
-        {dataSource.map((row, index) => (
+        {ordini.map((ordine, index) => (
           <TabellaRow
             key={index}
-            row={row}
-            disabled={dataSource.length === 1}
+            ordine={ordine}
+            disabled={ordini.length === 1}
             onRemoveRow={() => onRemoveRow(index)}
             onChangeValueRow={(key, value) =>
               onChangeValueRow(index, key, value)
@@ -36,44 +35,37 @@ const TabellaOrdini = ({ dataSource, onRemoveRow, onChangeValueRow }) => {
 
 export default TabellaOrdini;
 
-const TabellaRow = ({ row, disabled, onChangeValueRow, onRemoveRow }) => (
+const TabellaRow = ({ ordine, disabled, onChangeValueRow, onRemoveRow }) => (
   <Table.Row>
-    <Table.Cell width="2">
+    <Table.Cell width="6">
       <Input
-        value={row.name}
-        placeholder="Mario Rossi"
-        onChange={(e) => onChangeValueRow("name", e.target.value)}
-      />
-    </Table.Cell>
-    <Table.Cell width="4">
-      <Input
-        value={row.email}
-        placeholder="mariorossi@gmail.com"
-        onChange={(e) => onChangeValueRow("email", e.target.value)}
+        value={ordine.product}
+        placeholder="..."
+        onChange={(e) => onChangeValueRow("product", e.target.value)}
       />
     </Table.Cell>
     <Table.Cell width="2">
       <Input
-        value={row.color}
+        value={ordine.color}
         placeholder="#FFFFFF"
         onChange={(e) => onChangeValueRow("color", e.target.value)}
       />
     </Table.Cell>
     <Table.Cell width="2">
       <Input
-        value={row.code}
+        value={ordine.code}
         onChange={(e) => onChangeValueRow("code", e.target.value)}
       />
     </Table.Cell>
     <Table.Cell width="1">
       <InputNumber
-        value={row.quantity}
+        value={ordine.quantity}
         onChange={(value) => onChangeValueRow("quantity", value)}
       />
     </Table.Cell>
     <Table.Cell width="6">
       <Input
-        value={row.note}
+        value={ordine.note}
         onChange={(e) => onChangeValueRow("note", e.target.value)}
       />
     </Table.Cell>
